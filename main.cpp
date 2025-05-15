60fc2623375a86c3977173cda79922db7247c857
marciopablo <marciopablo014@gmail.com>
2025-05-14 23:47:27 -0300

    implementando tracinhos e verificando chutes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string> //garantir que todos os compiladores leiam a string. Boas praticas
+#include <map> //biblioteca para dicionario
 
 using namespace std;
 
 const string PALAVRA_SECRETA = "MARCIO"; //var levada a escopo global parra valer em tds os escopos. Const para boas praticas
+map<char, bool> chutou; //inplementacao do dicionario/vetor chutou
 
 bool letraExiste(char chute);
 
@@ -14,9 +16,20 @@ int main(){
     bool naoEnforcou = true;
 
     while(naoAcertou && naoEnforcou){
+        //verificar se o chute foi realizado com sucesso ou nao
+        for (char posicao : PALAVRA_SECRETA){
+            if(chutou[posicao]) {
+                cout << posicao << " "; //se chutou certo
+            }
+            else {
+                cout << "_ "; //se chutou errado
+            }
+        } cout << endl;
+
         char chute;
         cout << "Chute uma letra: "<< endl;
         cin >> chute;
+        chutou[chute] = true; //quando o chute for verdadeiro retorno isso ao map 
 
         if(letraExiste(chute)){
             cout << "Voce acertou. Seu chute esta na palavra." << endl;
