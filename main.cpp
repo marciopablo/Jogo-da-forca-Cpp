08c1ba0b5257295c3ec00ba5253854949b2f3ab3
marciopablo <marciopablo014@gmail.com>
2025-05-16 09:19:23 -0300

    mostrar chutes errados

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <string> //garantir que todos os compiladores leiam a string. Boas praticas
 #include <map> //biblioteca para dicionario
+#include <vector> //para vetor dinamico
 
 using namespace std;
 
 const string PALAVRA_SECRETA = "MARCIO"; //var levada a escopo global parra valer em tds os escopos. Const para boas praticas
 map<char, bool> chutou; //inplementacao do dicionario/vetor chutou
+vector<char> chuteErrado; //vetor dinamico para guardar chutes errados
 
 bool letraExiste(char chute);
 
 int main(){
-    cout << PALAVRA_SECRETA << endl;
+    cout << "*********************" << endl;
+    cout << "*** Jogo da Forca ***" << endl;
+    cout << "*********************" << endl;
+    cout << endl;
 
     bool naoAcertou = true;
     bool naoEnforcou = true;
 
     while(naoAcertou && naoEnforcou){
+        //printar os chutes errados (valores dentro do vetor dinamico)
+        cout << "Chutes errados: ";
+        for(char i : chuteErrado){
+            cout << i << " ";
+        }
+        cout << endl;
+        
         //verificar se o chute foi realizado com sucesso ou nao
         for (char posicao : PALAVRA_SECRETA){
             if(chutou[posicao]) {
@@ -36,7 +48,9 @@ int main(){
         }
         else {
             cout << "Voce errou. Seu chute nao esta na palavra." << endl;
+            chuteErrado.push_back(chute); //pega o valor da var chute que foi desviado para o else e guarda no fim do vetorr dinamico
         }
+        cout << endl;
     }
 }
 //como varrer uma string posicao por posicao
